c6f310ca223735c6cc1f483f90a7cb01ddf31551
Blue89Azul <life.89.blue@gmail.com>
2022-08-23 21:26:39 +0900

    update/add 2tests

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Tests\Feature;
-
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,5 +12,20 @@ class ArticleControllerTest extends TestCase
     public function testIndex() {
         $response = $this->get(route("articles.index"));
         $response->assertStatus(200)->assertViewIs("articles.index");
+    }
+
+    public function testGuestCreate() {
+        $response = $this->get(route("articles.create"));
+        $response->assertRedirect(route("login"));
+    }
+
+    public function testAuthCreate() {
+        $user = factory(User::class)->create();
+
+        $response = $this->actionAs($user)
+         ->get(route("articles.create"));
+
+        $response->assertStatus(200)
+         ->assertViewIs("articles.create");
     }
 }
